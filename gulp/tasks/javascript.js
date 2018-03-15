9e16ee47c204df211a46f077655da216e1039e83
Sergey Khmelevskoy <xs290@me.com>
2018-03-15 20:18:14 +0300

    fixed es uglify

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -3,7 +3,7 @@ var path        = require('path');
 var util        = require('gulp-util');
 var plumber     = require('gulp-plumber');
 var concat      = require('gulp-concat');
-var uglifyJs    = require('gulp-uglify');
+var uglifyJs    = require('gulp-uglify-es');
 var config      = require('../config');
 
 gulp.task('javascript:vendor', function() {
